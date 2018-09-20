72da7da93103bff3af7e474e638c8c592ea36cf4
Arnab Ghosh <43007068+ArnabG99@users.noreply.github.com>
2018-09-21 00:32:37 +0530

    Update ArmstrongInRange1000.py

--- ArmstrongInRange1000.py
+++ ArmstrongInRange1000.py
@@ -1,12 +1,10 @@
 for num in range(2,1000):
     copy = num
     sum = 0
+    print("Armstrong Numbers in range of 1000")
     while(copy>0):
         digit = copy % 10
-        copy = int(copy / 10)
+        copy = int(copy / 10) //Removing last digit.
         sum = sum + digit**3
     if(sum == num):
-        print(num)
-        
-
-        
+        print(num)               
