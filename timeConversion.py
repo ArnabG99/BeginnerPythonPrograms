6295b42cc052e9d1d32849adf6634c5c02714eae
Arnab Ghosh <43007068+ArnabG99@users.noreply.github.com>
2018-11-07 18:54:00 +0530

    Convert 12hr time to 24hr time

---
+++ timeConversion.py
@@ -0,0 +1,20 @@
+hr12 = "01:05:45AM"
+hr24 = ""
+
+h = int(hr12[0:2])
+m = int(hr12[3:5])
+s = int(hr12[6:8])
+tm = hr12[8:10]
+
+if(h==12 and tm=='PM'):
+    hr24 = str(format(h,"02"))+':'+str(format(m,"02"))+':'+str(format(s,"02"))
+elif(h==12 and tm=='AM'):
+    hr24 = str(format(00,"02"))+':'+str(format(m,"02"))+':'+str(format(s,"02"))
+elif(tm=='AM'):
+    hr24 = str(format(h,"02"))+':'+str(format(m,"02"))+':'+str(format(s,"02"))
+elif(tm=='PM'):
+    hr24 = str(format(h+12,"02"))+':'+str(format(m,"02"))+':'+str(format(s,"02"))
+else:
+    hr24="error"
+  
+print(hr24)
